2dcbefc55cdd75c2fd56f1bf75d4eebc1524275b
KhaoulaElmajni <50221822+KhaoulaElmajni@users.noreply.github.com>
2021-06-03 19:00:07 +0100

    Ajout des entités ‘Article’, ‘Comment’ et ‘Tag’
    Ajout des entités ‘Article’, ‘Comment’ et ‘Tag’

--- elmajni/routes/users.js
+++ elmajni/routes/users.js
@@ -1,19 +1,67 @@
-const router = require('express').Router();
- const usersRepo = require('../repositories/users')
- /* GET users listing. */
- router.get('/', async function(req, res, next) {
-   res.send(await usersRepo.getAllUsers())
- });
- module.exports = router;
 
-
-
-/*var express = require('express');
+var express = require('express');
 var router = express.Router();
+const usersRepo = require('../repositories/users')
 
-/* GET users listing. 
-router.get('/',async function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res, next) {
+   const usersNbr = req.query.usersNbr;
+   if(usersNbr == undefined)
+      res.send(await usersRepo.getAllUsers())
+   else
+      res.send(await usersRepo.getAllUsersNumber())
 });
 
-module.exports = router;*/
+router.get('/offset/:offset/limit/:limit', async function (req, res, next) {
+   const offset = req.params.offset
+   const limit = req.params.limit
+   res.send(await usersRepo.getUsers(offset, limit))
+});
+
+router.get('/admins', async function (req, res, next) {
+   res.send(await usersRepo.getAdmins())
+});
+
+router.get('/guests', async function (req, res, next) {
+   res.send(await usersRepo.getGuests())
+});
+
+router.get('/authors', async function (req, res, next) {
+   res.send(await usersRepo.getAuthors())
+});
+
+router.get('/:id', async function (req, res, next) {
+   const id = req.params.id;
+   const message = await usersRepo.getUser(id);
+   res.status(message.status).send(message)
+});
+
+router.get('/email/:email', async function (req, res, next) {
+   const email = req.params.email;
+   res.send(await usersRepo.getUserByEmail(email))
+});
+
+router.delete('/:id', async function (req, res, next) {
+   const id = req.params.id;
+   res.send(await usersRepo.deleteUser(id))
+});
+
+router.delete('/', async function (req, res, next) {
+   res.send(await usersRepo.deleteAllUsers())
+});
+
+router.post('/', async function (req, res, next) {
+   const user = req.body;
+   res.send(await usersRepo.addUser(user))
+});
+
+router.put('/:id', async function (req, res, next) {
+   const id = req.params.id;
+   const user = req.body;
+   res.send(await usersRepo.updateUser(id, user))
+});
+
+
+module.exports = router;
+
+
+
